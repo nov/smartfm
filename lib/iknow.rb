a83866e6b68bae548ed0569b78bcfb6a2ac4b2f1
nov <nov@bianca.cerego.co.jp>
2008-10-13 22:04:37 +0900

    library paths are fixed?

--- lib/iknow.rb
+++ lib/iknow.rb
@@ -6,6 +6,8 @@ require 'uri'
 require 'json'
 require 'date'
 
+require File.dirname(__FILE__) + '/../ext/hash'
+
 require File.dirname(__FILE__) + '/iknow/core'
 require File.dirname(__FILE__) + '/iknow/rest_client'
 require File.dirname(__FILE__) + '/iknow/model'
