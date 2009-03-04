54f08aeb9f7966dbed40bd10aa89d8ccb1ff3f5b
nov <nov@argentea.(none)>
2009-03-05 02:35:49 +0900

    removed User.username ..

--- lib/smartfm/rest_client/user.rb
+++ lib/smartfm/rest_client/user.rb
@@ -1,14 +1,13 @@
 class Smartfm::RestClient::User < Smartfm::RestClient::Base
 
   ACTIONS = {
-    :find          => { :path => '/users/__username__'              },
-    :lists         => { :path => '/users/__username__/lists'        },
-    :items         => { :path => '/users/__username__/items'        },
-    :friends       => { :path => '/users/__username__/friends'      },
-    :followers     => { :path => '/users/__username__/followers'    },
+    :find          => { :path => '/users/__username__'           },
+    :lists         => { :path => '/users/__username__/lists'     },
+    :items         => { :path => '/users/__username__/items'     },
+    :friends       => { :path => '/users/__username__/friends'   },
+    :followers     => { :path => '/users/__username__/followers' },
     :study_results => { :path => '/users/__username__/study_results/__application__'   },
-    :matching      => { :path => '/users/matching/__keyword__'      },
-    :username      => { :path => '/sessions', :http_method => :post }
+    :matching      => { :path => '/users/matching/__keyword__'   }
   }
 
 end
